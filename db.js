1a491ad41f7f9f773630a8b6b4afbc1dfaeed6e3
Daniel Pouzemski <danielpouzemski@Daniels-MacBook-Pro.local>
2017-01-22 14:00:54 +0100

    Mongo layer.

---
+++ db.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const collectionName = 'tasks';
+
+module.exports = {
+    connect() {
+        this.connected = new Promise((resolve, reject) => {
+            MongoClient.connect(process.env.MONGO_URL, (err, db) => {
+                if (err !== null) {
+                    console.log('connection error', error);
+                    reject(error);
+                    return;
+                }
+
+                console.log("Connected successfully to server");
+                resolve(db);
+            });
+        });
+    },
+
+    getTasks() {
+        return new Promise((resolve, reject) => {
+            this.connected.then(db => {
+                db.collection(collectionName).find({}, { '_id': false }).toArray((err, docs) => {
+                    if (err !== null) {
+                        reject(err);
+                        return;
+                    }
+
+                    resolve(docs);
+                });
+            });
+        });
+    }
+};
