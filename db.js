496698519cfbf4dd00114a0a90bc9b9a18aa141d
Yuriy Kurylenko <yuriy.kurylenko@gmail.com>
2017-01-30 22:39:35 +0100

    Removing extra stuff.

--- db.js
+++ db.js
@@ -4,43 +4,33 @@ const assert = require('assert');
 const collectionName = 'tasks';
 
 module.exports = {
-    connect() {
-        this.connected = new Promise((resolve, reject) => {
-            MongoClient.connect(process.env.MONGO_URL, (err, db) => {
-                if (err !== null) {
-                    console.log('connection error', error);
-                    reject(error);
-                    return;
-                }
+  connect() {
+    this.connected = new Promise((resolve, reject) => {
+      MongoClient.connect(process.env.MONGO_URL, (err, db) => {
+        if (err !== null) {
+          console.log('connection error', error);
+          reject(error);
+          return;
+        }
 
-                console.log("Connected successfully to server");
-                resolve(db);
-            });
-        });
-    },
+        console.log("Connected successfully to server");
+        resolve(db);
+      });
+    });
+  },
 
-    getTasks(filter) {
-      filter = filter || {};
-      return new Promise((resolve, reject) => {
-        this.connected.then(db => {
-          db.collection(collectionName).find(filter, { '_id': false }).toArray((err, docs) => {
-            if (err !== null) {
-                reject(err);
-                return;
-            }
-
-            resolve(docs);
-          });
+  getTasks(filter) {
+    filter = filter || {};
+    return new Promise((resolve, reject) => {
+      this.connected.then(db => {
+        db.collection(collectionName).find(filter, { '_id': false }).toArray((err, docs) => {
+          if (err !== null) {
+            reject(err);
+            return;
+          }
+          resolve(docs);
         });
       });
-    },
-
-    addTask(data) {
-      return new Promise((resolve, reject) => {
-        this.connected.then(db => {
-          db.collection(collectionName).insert(data);
-          resolve(data);
-        });
-      });
-    }
+    });
+  }
 };
--- index.js
+++ index.js
@@ -8,36 +8,13 @@ server.use(restify.queryParser());
 db.connect();
 
 server.get('/tasks', (req, res, next) => {
-  db.getTasks().then(
+  const filter = req.params.userId ? { assignee: Number(req.params.userId) } : {};
+  db.getTasks(filter).then(
     tasks => {
       res.send(200, tasks);
       next();
     },
     err => res.send(500, tasks)
-  );
-});
-
-server.get('/tasks/my', (req, res, next) => {
-  db.getTasks({ assignee: Number(req.params.userId) }).then(
-    tasks => {
-      res.send(200, tasks);
-      next();
-    },
-    err => res.send(500, tasks)
-  );
-});
-
-server.post('/tasks/my', (req, res, next) => {
-  var body = req.body;
-  body.assignee = body.userId;
-  delete body.userId;
-
-  db.addTask(body).then(
-    task => {
-      res.send(201, task);
-      next();
-    },
-    err => res.send(500)
   );
 });
 
